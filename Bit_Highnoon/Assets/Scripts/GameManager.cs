16e304844ece28a394208aa8a08e6b87edb0b9bd
SeoJiBeom <75297113+SeoJiBeom@users.noreply.github.com>
2020-12-08 17:22:16 +0900

    씬 singleton 추가
    singleton 추가 및 Intro -> Main 씬 전환 테스트

---
+++ Bit_Highnoon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance 
+    {
+        get
+        {
+            if(!instance)
+            {
+                instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                if (instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartMain()
+    {
+            SceneManager.LoadScene(1);
+    }
+}
