3abc2d2bdbab50b68d3ddca5feca4badc7741644
ttottotto111 <ksw4997@naver.com>
2020-12-08 17:23:45 +0900

    논리적DB 및 test용  class
    논리적 db생성 및 xml파일 저장 불러오기
    사용자 추가 및 삭제
    난이도 진도 변경기능
    추가할내용 : 전적 카운트예정

---
+++ Bit_Highnoon/Assets/DB/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    LogicalDB db = new LogicalDB();
+    // Start is called before the first frame update
+    void Start()
+    {
+        db.CreateTable();
+        db.InsertUser("aaa", "red");
+        db.InsertUser("bbb", "blue");
+        db.NormalUser(1);
+        db.DeleteUser(2);
+        db.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
