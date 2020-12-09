128e5dccee0a34addd45eb3940fe878ad390696f
hdh980217 <74037045+hdh980217@users.noreply.github.com>
2020-12-09 17:28:15 +0900

    Animator 추가 및 Animation 적용

--- Bit_Highnoon/Assets/Scripts/AI/AIEasy.cs
+++ Bit_Highnoon/Assets/Scripts/AI/AIEasy.cs
@@ -11,5 +11,34 @@ public class AIEasy : AIParent
         idletime = 5;   //대기시간
             
         deadtime = 20;  //플레이어가 죽는 시간
+
+        StartCoroutine(CheckState());               //상태를 체크
+        StartCoroutine(CheckStateForAction());      //상태의 따른 행동
+    }
+
+    protected override void IdleAction()
+    {
+        base.IdleAction();
+
+        animator.SetTrigger("drink");
+    }
+
+    protected override void AttackAction()
+    {
+        base.AttackAction();
+
+        animator.SetTrigger("attack");
+    }
+
+
+    private void Hit()
+    {
+        animator.SetTrigger("hit");
+    }
+
+    private void Dead()
+    {
+        animator.SetTrigger("dead");
+        isdead = true;
     }
 }
