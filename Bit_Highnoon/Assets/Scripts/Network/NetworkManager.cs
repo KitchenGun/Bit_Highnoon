88c9369a108b6ed17daddafb526ec94e8727a825
akdl1604 <57994912+akdl1604@users.noreply.github.com>
2020-12-11 20:28:06 +0900

    Photon assets 추가

---
+++ Bit_Highnoon/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, false);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+    }
+}
