ad7f5659f3a7db366b5be2967f5c8b01b1e49faa
KitchenGun <kitchengun124@gmail.com>
2020-12-14 11:24:22 +0900

    FirePos 제작 Enemy 모델링 텍스쳐 애니메이션 추가

---
+++ Bit_Highnoon/Assets/Scripts/Character/HandGunRayCast.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunRayCast : MonoBehaviour
+{
+    private GameObject FirePos; // Ray가  시작할 위치 정보를 가진 오브젝트
+    
+
+    void Start()
+    {
+        FirePos = this.gameObject.transform.Find("GunFirePos").gameObject;
+        Debug.Log(FirePos.tag);
+    }
+
+    void Update()
+    {
+        Debug.Log(FirePos.tag);
+    }
+}
