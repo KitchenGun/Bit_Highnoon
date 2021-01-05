a6369c54c508de09f18802f4be2fdf9f0a1de1e5
KitchenGun <kitchengun124@gmail.com>
2021-01-05 17:26:21 +0900

    Revert "Merge branch 'SceneManager' into main"
    This reverts commit 4830706c303fc9afe2238ee2bcaae5ff26b8374b, reversing
    changes made to 00185b5c754a28ae47f4d334e676e5e21c8d4f27.

--- Bit_Highnoon/Assets/Scripts/SceneManager/StartIntro.cs
+++ Bit_Highnoon/Assets/Scripts/SceneManager/StartIntro.cs
@@ -7,62 +7,19 @@ public class StartIntro : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public GameObject bullet1;
-    public GameObject bullet2;
-    public GameObject bullet3;
-    public GameObject logo;
-    public Light pointlight;
-    
+    #region 시간 지연 씬 전환
     void Start()
     {
-        Invoke("InvokeBullet1", 1.0f);
-        Invoke("InvokeBullet2", 2.0f);
-        Invoke("InvokeBullet3", 2.5f);
-
-        Invoke("InvokeLogo", 3.0f);
-        Invoke("InvokeLight", 3.0f);
-
         Invoke("InvokeMain", 5f);
     }
 
-    #region 시간 지연 씬 전환
     void InvokeMain()
     {
         //현재 인덱스의 다음 인덱스값 저장
         int idx = SceneManager.GetActiveScene().buildIndex + 1;
- 
+
         //씬 넘기는 싱글톤 함수로 인덱스값 전달
         GameManager.Instance.ChangeToScene(idx);
     }
     #endregion
-
-    #region 탄 자국 나타나게 하기
-    void InvokeBullet1()
-    {
-        bullet1.SetActive(true);
-    }
-    void InvokeBullet2()
-    {
-        bullet2.SetActive(true);
-    }
-    void InvokeBullet3()
-    {
-        bullet3.SetActive(true);
-    }
-
-    void InvokeLogo()
-    {
-        logo.SetActive(true);   
-    }
-    #endregion
-
-    void InvokeLight()
-    {
-        //pointlight = GetComponent<Light>();
-        
-        for(int i = 0; i < 10; i++)
-        {
-            pointlight.intensity = pointlight.intensity + 0.1f;
-        }
-    }
 }
