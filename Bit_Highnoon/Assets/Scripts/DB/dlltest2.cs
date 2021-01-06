2936f6f3d81d0686c51437151601c7ff14ac1fe8
ttottotto111 <ksw4997@naver.com>
2021-01-06 19:00:40 +0900

    dll 테스트
    수정중

--- Bit_Highnoon/Assets/Scripts/DB/dlltest2.cs
+++ Bit_Highnoon/Assets/Scripts/DB/dlltest2.cs
@@ -7,22 +7,33 @@ using UnityEngine;
 public class dlltest2 : MonoBehaviour
 {
     public string ConMsg;
-
     // Start is called before the first frame update
     void Start()
     {
-        ConMsg = @"Data Source=DESKTOP-R8F9OUG\SQLEXPRESS;Initial Catalog=DeadEye;User ID=ksw;Password=123";
-        try
-        {
-            SqlConnection sc = new SqlConnection(ConMsg);
-            sc.Open();
-            sc.Close();
-        }
-        catch(Exception)
-        {
+         ConMsg = @"Data Source = DESKTOP-R8F9OUG\SQLEXPRESS; 
+     user id = ksw;
+     password = 123;
+     Initial Catalog = DeadEye;";
+        SimpleQuery("SELECT * FROM UserInfo WHERE UserID = 1");
+    }
 
+    public string SimpleQuery(string _query)
+    {
+        using (SqlConnection dbCon = new SqlConnection(ConMsg))
+        {
+            SqlCommand cmd = new SqlCommand(_query, dbCon);
+            try
+            {
+                dbCon.Open();
+                string _returnQuery = (string)cmd.ExecuteScalar();
+                return _returnQuery;
+            }
+            catch (SqlException _exception)
+            {
+                Debug.LogWarning(_exception.ToString());
+                return null;
+            }
         }
-        
     }
 
 }
