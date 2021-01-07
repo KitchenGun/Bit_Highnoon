07e8f0a9fae0ffc47132002ea5f513bf799f8ccb
akdl1604 <57994912+akdl1604@users.noreply.github.com>
2021-01-07 17:03:50 +0900

    멀티씬 작업

--- Bit_Highnoon/Assets/Scripts/Network/PlayerControl.cs
+++ Bit_Highnoon/Assets/Scripts/Network/PlayerControl.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using InputTracking = UnityEngine.XR.InputTracking;
+using Node = UnityEngine.XR.XRNode;
 
 public class PlayerControl : MonoBehaviourPunCallbacks
 {
-    public Transform head;
+    public GameObject ovrCamRig;
     public Transform leftHand;
     public Transform rightHand;
+    public Camera leftEye;
+    public Camera rightEye;
+    Vector3 pos;
+    public float speed = 3;
 
     PhotonView PV;
 
@@ -19,18 +25,59 @@ public class PlayerControl : MonoBehaviourPunCallbacks
     }
     void Start()
     {
+        pos = transform.position;
     }
     void Update()
     {
-        if (PV.IsMine)
+        if (!PV.IsMine)
         {
-            rightHand.gameObject.SetActive(false);
-            leftHand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
+            Destroy(ovrCamRig);
+        }
+        else
+        {
+            if(leftEye.tag != "MainCamera")
+            {
+                leftEye.tag = "MainCamera";
+                leftEye.enabled = true;
+            }
+            if (rightEye.tag != "MainCamera")
+            {
+                rightEye.tag = "MainCamera";
+                rightEye.enabled = true;
+            }
+            leftHand.localRotation = InputTracking.GetLocalRotation(Node.LeftHand);
+            rightHand.localRotation = InputTracking.GetLocalRotation(Node.RightHand);
 
-            MapPosition(transform, XRNode.Head);
-            MapPosition(transform, XRNode.LeftHand);
-            MapPosition(transform, XRNode.RightHand);
+            leftHand.localPosition = InputTracking.GetLocalPosition(Node.LeftHand);
+            rightHand.localPosition = InputTracking.GetLocalPosition(Node.RightHand);
+
+            Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+            if(primaryAxis.y > 0f)
+            {
+                pos += (primaryAxis.y * transform.forward * Time.deltaTime * speed);
+            }
+            if(primaryAxis.y < 0f)
+            {
+                pos += (Mathf.Abs(primaryAxis.y) * -transform.forward * Time.deltaTime * speed);
+            }
+
+            if (primaryAxis.x > 0f)
+            {
+                pos += (primaryAxis.x * transform.forward * Time.deltaTime * speed);
+            }
+            if (primaryAxis.x < 0f)
+            {
+                pos += (Mathf.Abs(primaryAxis.x) * -transform.forward * Time.deltaTime * speed);
+            }
+
+            transform.position = pos;
+
+            Vector3 euler = transform.rotation.eulerAngles;
+            Vector2 secondaryAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+            euler.y += secondaryAxis.x;
+            transform.rotation = Quaternion.Euler(euler);
+            transform.localRotation = Quaternion.Euler(euler);
         }
     }
     void MapPosition(Transform target,XRNode node)
