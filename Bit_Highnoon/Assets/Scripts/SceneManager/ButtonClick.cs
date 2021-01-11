771640bffa74b96473f3ee3d4292a0f5ef01ca30
SeoJiBeom <75297113+SeoJiBeom@users.noreply.github.com>
2021-01-11 14:52:35 +0900

    버튼 코드 틀 구성

--- Bit_Highnoon/Assets/Scripts/SceneManager/ButtonClick.cs
+++ Bit_Highnoon/Assets/Scripts/SceneManager/ButtonClick.cs
@@ -7,6 +7,37 @@ public class ButtonClick : MonoBehaviour
 {
    public void Hit()
     {
-        
+        if (this.gameObject.name == "Find")
+        {
+
+        }
+        else if (this.gameObject.name == "Create")
+        {
+
+        }
+        else if (this.gameObject.name == "Quit")
+        {
+
+        }
+        else if (this.gameObject.name == "CreateRoom")
+        {
+
+        }
+        else if (this.gameObject.name == "LeaveRoom")
+        {
+
+        }
+        else if (this.gameObject.name == "StartRoom")
+        {
+
+        }
+        else if (this.gameObject.name == "OK")
+        {
+
+        }
+        else if (this.gameObject.name == "Back")
+        {
+
+        }
     }
 }
