f8f053ab1e6b9d560b0826ce1a2a2b61c3fa32f2
SeoJiBeom <75297113+SeoJiBeom@users.noreply.github.com>
2021-01-12 11:28:14 +0900

    레이캐스트로 버튼 클릭

--- Bit_Highnoon/Assets/Scripts/SceneManager/ButtonClick.cs
+++ Bit_Highnoon/Assets/Scripts/SceneManager/ButtonClick.cs
@@ -3,41 +3,41 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+
 public class ButtonClick : MonoBehaviour
 {
-   public void Hit()
+    public void Hit()
     {
+        MenuManager mm = GameObject.Find("Canvas").GetComponent<MenuManager>();
+        NetworkManager nm = GameObject.Find("Canvas").GetComponent<NetworkManager>();
+
         if (this.gameObject.name == "Find")
         {
-
+            mm.OpenMenu("FindRoomMenu");
         }
         else if (this.gameObject.name == "Create")
         {
-
-        }
-        else if (this.gameObject.name == "Quit")
-        {
-
+            mm.OpenMenu("CreateRoomMenu");
         }
         else if (this.gameObject.name == "CreateRoom")
         {
-
+            nm.CreateRoom();
         }
         else if (this.gameObject.name == "LeaveRoom")
         {
-
+            nm.LeaveRoom();
         }
         else if (this.gameObject.name == "StartRoom")
         {
-
+            nm.StartGame();
         }
         else if (this.gameObject.name == "OK")
         {
-
+            mm.OpenMenu("TitleMenu");
         }
         else if (this.gameObject.name == "Back")
         {
-
+            mm.OpenMenu("TitleMenu");
         }
     }
 }
