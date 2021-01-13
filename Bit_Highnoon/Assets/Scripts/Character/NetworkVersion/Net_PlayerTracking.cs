33edf92a36edd34455811c2e86761aff011bf4ef
KitchenGun <kitchengun124@gmail.com>
2021-01-13 11:16:43 +0900

    Update NetPlayer PlayerTracking
    Update문에 PV.ismine추가, 자신이 아닐경우 ovr매니져도 종료

--- Bit_Highnoon/Assets/Scripts/Character/NetworkVersion/Net_PlayerTracking.cs
+++ Bit_Highnoon/Assets/Scripts/Character/NetworkVersion/Net_PlayerTracking.cs
@@ -7,6 +7,7 @@ public class Net_PlayerTracking : MonoBehaviourPunCallbacks
 {
     private PhotonView PV;
     private OVRCameraRig ovrCamRig;
+    private OVRManager ovrManager;
     [SerializeField]
     private GameObject Head; //머리의 위치정보를 담고 있는 오브젝트
     private Camera HeadCam;
@@ -17,6 +18,7 @@ public class Net_PlayerTracking : MonoBehaviourPunCallbacks
     private void Awake()
     {
         ovrCamRig = this.gameObject.transform.parent.Find("OVRCameraRig").gameObject.GetComponent<OVRCameraRig>();
+        ovrManager = this.gameObject.transform.parent.Find("OVRCameraRig").gameObject.GetComponent<OVRManager>();
         PV = this.gameObject.transform.parent.GetComponent<PhotonView>();
     }
     void Start()
@@ -29,6 +31,8 @@ public class Net_PlayerTracking : MonoBehaviourPunCallbacks
         }
         else
         {
+            ovrCamRig.enabled = false;
+            ovrManager.enabled = false;
             HeadCam.transform.gameObject.tag = "Untagged";
             HeadCam.enabled = false;
         }
@@ -36,26 +40,29 @@ public class Net_PlayerTracking : MonoBehaviourPunCallbacks
 
     void Update()
     {
-        #region Body 회전과 위치 값 전달
-        Body.transform.position = new Vector3
-            (Head.transform.position.x,
-            0,//이동 기능 제작시 플레이어의 최상위 오브젝트의 위치값을 받아오도록 제작
-            Head.transform.position.z);
-        Body.transform.eulerAngles = new Vector3(0, Head.transform.rotation.eulerAngles.y, 0); // 회전값 y축 만 전달
-        #endregion
-        #region 크기 변화
-        Body.transform.localScale = new Vector3(
-            Body.transform.localScale.x,
-            (float)(Head.transform.position.y) / 2,
-            Body.transform.localScale.z);
-        #endregion
-        #region  벨트 위치 회전값 변경
-        Belt.transform.position = new Vector3(
-            Body.transform.position.x,
-            (float)(Head.transform.position.y) / 3 * 2,
-            Body.transform.position.z);
-        Belt.transform.eulerAngles = new Vector3(0, Head.transform.rotation.eulerAngles.y, 0); // 회전값 y축 만 전달
-        #endregion
+        if (PV.IsMine)
+        {
+            #region Body 회전과 위치 값 전달
+            Body.transform.position = new Vector3
+                (Head.transform.position.x,
+                0,//이동 기능 제작시 플레이어의 최상위 오브젝트의 위치값을 받아오도록 제작
+                Head.transform.position.z);
+            Body.transform.eulerAngles = new Vector3(0, Head.transform.rotation.eulerAngles.y, 0); // 회전값 y축 만 전달
+            #endregion
+            #region 크기 변화
+            Body.transform.localScale = new Vector3(
+                Body.transform.localScale.x,
+                (float)(Head.transform.position.y) / 2,
+                Body.transform.localScale.z);
+            #endregion
+            #region  벨트 위치 회전값 변경
+            Belt.transform.position = new Vector3(
+                Body.transform.position.x,
+                (float)(Head.transform.position.y) / 3 * 2,
+                Body.transform.position.z);
+            Belt.transform.eulerAngles = new Vector3(0, Head.transform.rotation.eulerAngles.y, 0); // 회전값 y축 만 전달
+            #endregion
+        }
     }
 
     #region 벨트 오브젝트 반환
