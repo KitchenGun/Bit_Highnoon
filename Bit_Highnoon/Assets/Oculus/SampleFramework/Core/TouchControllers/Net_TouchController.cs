7218170609a30af1b76dc93bbb80b7018dc083a4
KitchenGun <kitchengun124@gmail.com>
2021-01-18 14:26:06 +0900

    Grab함수 Network 캐릭터용으로 수정
    Object.Instantiate 대신 PhotonNetwork.Instantiate 를 사용

--- Bit_Highnoon/Assets/Oculus/SampleFramework/Core/TouchControllers/Net_TouchController.cs
+++ Bit_Highnoon/Assets/Oculus/SampleFramework/Core/TouchControllers/Net_TouchController.cs
@@ -11,6 +11,7 @@ language governing permissions and limitations under the license.
 
 using UnityEngine;
 using Photon.Pun;
+using System.IO;
 
 namespace OVRTouchSample
 {
@@ -19,35 +20,27 @@ namespace OVRTouchSample
     {
         private GameManager GM;
         private PhotonView PV;
+        private int PVN;
 
-        #region 기존 오큘러스 컨트롤러 스크립트 변수
         [SerializeField]
         private OVRInput.Controller m_controller = OVRInput.Controller.None;
         [SerializeField]
         private Animator m_animator = null;
 
         private bool m_restoreOnInputAcquired = false;
-        #endregion
 
-        #region Grab&Drop
         private string side; //컨트롤러 방향
         private bool isHandOnColider;//손이 이벤트 오브젝트와 충돌하는가?
 
-        public GameObject GunSample; //무장하고 있는 총 샘플
         [SerializeField]
         private GameObject Belt; //밸트
-        #endregion
 
-        #region Fire 가능 확인 변수
         private bool FireState;
         private int Bullet;
-        #endregion
 
-        #region AudioSource
         private AudioSource HandAudio;
         private AudioClip Pistol_Garb;
         private AudioClip Pistol_DropOnHolster;
-        #endregion
 
         private void Awake()
         {
@@ -56,7 +49,6 @@ namespace OVRTouchSample
 
         private void Start()
         {
-            //PV = this.gameObject.GetPhotonView();//this.gameObject.transform.parent.gameObject.GetComponent<PhotonView>();
             GM = GameObject.Find("GameManager").GetComponent<GameManager>();
             HandAudio = this.gameObject.GetComponent<AudioSource>();//오디오 소스 선택
             side = this.gameObject.tag;//현재 컨트롤러 오른쪽 왼쪽 확인용
@@ -68,28 +60,22 @@ namespace OVRTouchSample
         {
             if (PV.IsMine)
             {
-                #region 기존 컨트롤러 애니메이션 (사용안함)
+
                 //m_animator.SetFloat("Button 1", OVRInput.Get(OVRInput.Button.One, m_controller) ? 1.0f : 0.0f);
                 //m_animator.SetFloat("Button 2", OVRInput.Get(OVRInput.Button.Two, m_controller) ? 1.0f : 0.0f);
                 //m_animator.SetFloat("Joy X", OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, m_controller).x);
                 //m_animator.SetFloat("Joy Y", OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, m_controller).y);
                 //m_animator.SetFloat("Grip", OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, m_controller));
                 //m_animator.SetFloat("Trigger", OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, m_controller));
-                #endregion
 
-                #region 컨트롤러 연결 확인
                 OVRManager.InputFocusAcquired += OnInputFocusAcquired;
                 OVRManager.InputFocusLost += OnInputFocusLost;
-                #endregion
 
-                #region 총 들고있는지 확인
                 DropCheck();
-                #endregion
 
             }
         }
 
-        #region 컨트롤러 연결 확인
         private void OnInputFocusLost()
         {
             if (gameObject.activeInHierarchy)
@@ -107,9 +93,7 @@ namespace OVRTouchSample
                 m_restoreOnInputAcquired = false;
             }
         }
-        #endregion
 
-        #region 충돌처리
         private void OnCollisionStay(Collision collision)//벨트에서 총을 뽑을때
         {
             if (!PV.IsMine)
@@ -276,7 +260,7 @@ namespace OVRTouchSample
         private void GrapGun_Ground(int ViewID)
         {
             PhotonView PVN = PhotonView.Find(ViewID);
-            GameObject gun = PVN.gameObject;
+            GameObject gun = PVN.transform.gameObject;
 
             //총에 정보에 접근 
             getGunInfo(gun);
@@ -309,9 +293,7 @@ namespace OVRTouchSample
                 isHandOnColider = false;
             }
         }
-        #endregion
 
-        #region 교체 함수
         private void HandtoGun()
         {
             //컨트롤러->총으로 모델링 교체
@@ -327,9 +309,7 @@ namespace OVRTouchSample
             this.gameObject.transform.Find("OculusTouchForQuest2").GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
             this.gameObject.transform.Find("gun_hand").gameObject.SetActive(false);
         }
-        #endregion
 
-        #region 총 들고있는지 확인
         private void DropCheck()
         {
             if(!isHandOnColider)
@@ -358,34 +338,15 @@ namespace OVRTouchSample
         private void DropGun()
         {
             //드랍 할 경우 손 위치에 총 모양 생성
-            GameObject Gun = Instantiate<GameObject>(GunSample, this.transform.position, Quaternion.identity) as GameObject;
+            GameObject Gun = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Net_Gun"), this.transform.position, Quaternion.identity, 0, new object[] { PV.ViewID });
+            Gun.name = "Gun";
             Gun.tag = "DropObj";
+            //Gun.GetPhotonView().SetOwnerInternal(photonView.Owner, photonView.ViewID);
             setGunInfo(Gun);//총기 설정 저장
                             //컨트롤러로 교체
             GuntoHand();
         }
-        #endregion
 
-        #region 강제 Drop
-        private void Drop()
-        {
-            this.gameObject.GetComponent<SphereCollider>().enabled = false;
-            if (this.gameObject.transform.Find("OculusTouchForQuest2").GetComponentInChildren<SkinnedMeshRenderer>().enabled == false)
-            {
-                //총을 들고있을 경우
-                this.gameObject.GetComponent<SphereCollider>().enabled = false;
-                //드랍 할 경우 손 위치에 총 모양 생성
-                GameObject Gun = Instantiate<GameObject>(GunSample, this.transform.position, Quaternion.identity) as GameObject;
-                Gun.tag = "DropObj";
-                setGunInfo(Gun);//총기 설정 저장
-                //컨트롤러로 교체
-                GuntoHand();
-            }
-           
-        }
-        #endregion
-
-        #region 총의 정보를 넣고 빼는 함수
         private void getGunInfo(GameObject gun)
         {
             //총에 정보에 접근 
@@ -399,6 +360,6 @@ namespace OVRTouchSample
             gun.GetComponent<Net_Revolver>().setFireState(this.FireState);
             gun.GetComponent<Net_Revolver>().setbullet(this.Bullet,false);
         }
-        #endregion
+
     }
 }
--- Bit_Highnoon/Assets/Scripts/Character/NetworkVersion/Gun/Net_Revolver.cs
+++ Bit_Highnoon/Assets/Scripts/Character/NetworkVersion/Gun/Net_Revolver.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using Photon.Pun;
 
 public class Net_Revolver : MonoBehaviour
+
 {
     private PhotonView PV;
     private GameManager GM;
-    private bool NewGun = true;
+    private bool NewGun = true; 
     int Max_bullet=6;
     public int cur_bullet { get; private set; }
     public bool FireState { get; private set; }
@@ -19,13 +20,13 @@ public class Net_Revolver : MonoBehaviour
     private void Start()
     {
         PV = this.gameObject.GetPhotonView();
-            GM = GameObject.Find("GameManager").GetComponent<GameManager>();
-            if (this.gameObject.name != "Gun(Clone)")
-            {
-                setbullet(Max_bullet, NewGun);
-                setFireState(true);
-            }
-            HitOtherObjAudio = this.gameObject.GetComponent<AudioSource>();
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if (this.gameObject.name != "Gun(Clone)")
+        {
+            setbullet(Max_bullet, NewGun);
+            setFireState(true);
+        }
+        HitOtherObjAudio = this.gameObject.GetComponent<AudioSource>();
     }
     public void setbullet(int bullet,bool isNewGun)
     {
@@ -58,15 +59,16 @@ public class Net_Revolver : MonoBehaviour
     #region 바닥충돌 감지
     private void OnCollisionEnter(Collision collision)
     {
-      
-            if (this.gameObject.name != "BeltGun")
+
+        if (this.gameObject.name != "BeltGun")
+        {
+            if (collision.gameObject.layer == 8)
             {
-                if (collision.gameObject.layer == 8)
-                {
-                    HitOtherObjAudio.clip = GM.GetComponent<GameManager>().LoadAudioClip("drop");
-                    HitOtherObjAudio.Play();
-                }
+                HitOtherObjAudio.clip = GM.GetComponent<GameManager>().LoadAudioClip("drop");
+                HitOtherObjAudio.Play();
             }
+        }
     }
     #endregion
+
 }
