6173744385aa2cfe36c1d5ab434156053b67c21f
hdh980217 <74037045+hdh980217@users.noreply.github.com>
2021-01-20 15:57:54 +0900

    Hat Material 랜덤 생성

---
+++ Bit_Highnoon/Assets/Scripts/Character/NetworkVersion/HatMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatMaterial : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] materials;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<SkinnedMeshRenderer>().sharedMaterial = materials[Random.Range(0, materials.Length)];
+    }
+}
