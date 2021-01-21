67d64d6b0ab0b884330f82139061f04b2b0217a0
SeoJiBeom <75297113+SeoJiBeom@users.noreply.github.com>
2021-01-21 16:39:53 +0900

    로비 애니메이터 조정

---
+++ Bit_Highnoon/Assets/Scripts/SceneManager/NpcDelay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDelay : MonoBehaviour
+{
+    private Animator animator;
+    private float Timer;
+    private bool tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        tr = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tr == true)
+        {
+            Timer = Timer + Time.deltaTime;
+        }
+
+        //if (this.gameObject.name == "card_npc_1")
+        //{
+            if (Timer > 10.0f)
+            {
+                animator.SetInteger("New Int", 10);
+            }
+
+            if (Timer > 40.0f)
+            {
+                animator.SetInteger("New Int", 40);
+            }
+
+            if (Timer > 50.0f)
+            {
+                animator.SetInteger("New Int", 50);
+            }
+
+            if (Timer > 80.0f)
+            {
+                animator.SetInteger("New Int", 80);
+                Timer = 0.0f;
+            }
+        //}
+    }
+}
