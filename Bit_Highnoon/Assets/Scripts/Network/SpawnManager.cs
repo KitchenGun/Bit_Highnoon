68b145d890f2ca3419f98befff3e64817b878ecf
hdh980217 <74037045+hdh980217@users.noreply.github.com>
2021-01-26 11:52:49 +0900

    스폰 방식 원래대로 수정

--- Bit_Highnoon/Assets/Scripts/Network/SpawnManager.cs
+++ Bit_Highnoon/Assets/Scripts/Network/SpawnManager.cs
@@ -9,39 +9,14 @@ public class SpawnManager : MonoBehaviour
 
     Spawnpoint[] spawnpoints;
 
-    PhotonView PV;
-
-    int count;
-
     void Awake()
     {
         instance = this;
-        PV = this.gameObject.GetPhotonView();
         spawnpoints = GetComponentsInChildren<Spawnpoint>();
-
-        IsCount();
     }
 
     public Transform GetSpawnpoint()
     {
-        Transform spawnpoint = spawnpoints[count].transform;
-
-        PV.RPC("Count", RpcTarget.AllBuffered);
-
-        return spawnpoint;
-    }
-
-    [PunRPC]
-    private void Count()
-    {
-        count++;
-    }
-
-    private void IsCount()  //카운트의 값을 판단한다.
-    {
-        if (count >= 1)
-        { }
-        else
-            count = 0;
+        return spawnpoints[Random.Range(0, spawnpoints.Length)].transform;
     }
 }
