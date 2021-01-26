c897b5c3ccef808b1eb4571d17e1310b758e6534
akdl1604 <57994912+akdl1604@users.noreply.github.com>
2021-01-26 14:35:57 +0900

    로비매니저 작성

--- Bit_Highnoon/Assets/LobbyNetwork.cs
+++ Bit_Highnoon/Assets/LobbyNetwork.cs
@@ -7,13 +7,25 @@ using UnityEngine;
 
 public class LobbyNetwork : MonoBehaviourPunCallbacks
 {
-    [SerializeField] TMP_InputField NickNmaeInput;
+    [SerializeField] TMP_InputField NickNameInput; //아이디 입력 텍스트
 
+    #region 서버접속 & 연결
     public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("서버접속완료");
-        PhotonNetwork.LocalPlayer.NickName = NickNmaeInput.text;
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        Debug.Log(NickNameInput + "서버접속완료");
+        JoinLobby();
     }
+    #endregion
+
+    #region 로비접속
+    public void JoinLobby() => PhotonNetwork.JoinLobby();
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비접속 완료");
+    }
+    #endregion
 }
