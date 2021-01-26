10fba2e81d0fd7efd465c7f3d6872fb07cb511c7
KitchenGun <kitchengun124@gmail.com>
2021-01-26 14:37:21 +0900

    mat 변경 스크립트

---
+++ Bit_Highnoon/Assets/Scripts/Character/ColorChangeTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeTest : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] CharacterMaterial;
+    public string Colorstr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Material mat in CharacterMaterial)
+        {
+            if(Colorstr==mat.name)
+            {
+                this.gameObject.GetComponent<Renderer>().material =mat;
+            }
+        }
+    }
+}
