af786370edbf5ed1fc1eb9f803f5acc1f184321c
KitchenGun <kitchengun124@gmail.com>
2021-01-26 10:45:26 +0900

    PianoSound추가

---
+++ Bit_Highnoon/Assets/Scripts/SceneManager/LobbySound/PianoLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoLoop : MonoBehaviour
+{
+    private AudioSource piano;
+    private GameManager GM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        piano = this.gameObject.GetComponent<AudioSource>();
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        piano.clip = GM.GetComponent<GameManager>().LoadAudioClip("piano");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!piano.isPlaying)
+            ClipChange();
+    }
+    private void ClipChange()
+    {
+        piano.clip = GM.GetComponent<GameManager>().LoadAudioClip("piano");
+        piano.Play();
+    }
+
+}
