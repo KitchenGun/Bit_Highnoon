f196c377e5990a584705497e251eeb9556c30da3
hdh980217 <74037045+hdh980217@users.noreply.github.com>
2021-01-26 11:41:17 +0900

    스폰 테스트2

--- Bit_Highnoon/Assets/Scripts/Network/SpawnManager.cs
+++ Bit_Highnoon/Assets/Scripts/Network/SpawnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -8,20 +9,39 @@ public class SpawnManager : MonoBehaviour
 
     Spawnpoint[] spawnpoints;
 
-    int count = 0;
+    PhotonView PV;
+
+    int count;
 
     void Awake()
     {
         instance = this;
+        PV = this.gameObject.GetPhotonView();
         spawnpoints = GetComponentsInChildren<Spawnpoint>();
+
+        IsCount();
     }
 
     public Transform GetSpawnpoint()
     {
         Transform spawnpoint = spawnpoints[count].transform;
 
-        count++;
+        PV.RPC("Count", RpcTarget.AllBuffered);
 
         return spawnpoint;
+    }
+
+    [PunRPC]
+    private void Count()
+    {
+        count++;
+    }
+
+    private void IsCount()  //카운트의 값을 판단한다.
+    {
+        if (count >= 1)
+        { }
+        else
+            count = 0;
     }
 }
