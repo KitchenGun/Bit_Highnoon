0951f76a7f75276ee6ef4790382b66a13f3069da
akdl1604 <57994912+akdl1604@users.noreply.github.com>
2021-02-01 11:07:36 +0900

    랜덤입장과 씬서버 정보 출력추가

--- Bit_Highnoon/Assets/Scripts/Network/PlayerManager.cs
+++ Bit_Highnoon/Assets/Scripts/Network/PlayerManager.cs
@@ -24,6 +24,7 @@ public class PlayerManager : MonoBehaviour
             CreateController();
         }
     }
+    #region 플레이어 생성
     void CreateController()
     {
         /*Transform spawnpoint = SpawnManager.instance.GetSpawnpoint();
@@ -31,4 +32,30 @@ public class PlayerManager : MonoBehaviour
         */
         controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "NetworkPlayer"),transform.position,transform.rotation);
     }
+    #endregion
+
+    #region 방정보보기
+    [ContextMenu("정보")]
+    void Info()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            print("현재 방 이름 : " + PhotonNetwork.CurrentRoom.Name);
+            print("현재 방 인원수 : " + PhotonNetwork.CurrentRoom.PlayerCount);
+            print("현재 방 최대인원수 : " + PhotonNetwork.CurrentRoom.MaxPlayers);
+
+            string playerStr = "방에 있는 플레이어 목록 : ";
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++) playerStr += PhotonNetwork.PlayerList[i].NickName + ", ";
+            print(playerStr);
+        }
+        else
+        {
+            print("접속한 인원 수 : " + PhotonNetwork.CountOfPlayers);
+            print("방 개수 : " + PhotonNetwork.CountOfRooms);
+            print("모든 방에 있는 인원 수 : " + PhotonNetwork.CountOfPlayersInRooms);
+            print("로비에 있는지? : " + PhotonNetwork.InLobby);
+            print("연결됐는지? : " + PhotonNetwork.IsConnected);
+        }
+    }
+    #endregion
 }
--- Bit_Highnoon/Assets/Scripts/Network/RoomManagers.cs
+++ Bit_Highnoon/Assets/Scripts/Network/RoomManagers.cs
@@ -87,4 +87,29 @@ public class RoomManagers : MonoBehaviourPunCallbacks
         myListRenewal();
     }
     #endregion
+
+    #region 로비정보보기
+    [ContextMenu("정보")]
+    void Info()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            print("현재 방 이름 : " + PhotonNetwork.CurrentRoom.Name);
+            print("현재 방 인원수 : " + PhotonNetwork.CurrentRoom.PlayerCount);
+            print("현재 방 최대인원수 : " + PhotonNetwork.CurrentRoom.MaxPlayers);
+
+            string playerStr = "방에 있는 플레이어 목록 : ";
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++) playerStr += PhotonNetwork.PlayerList[i].NickName + ", ";
+            print(playerStr);
+        }
+        else
+        {
+            print("접속한 인원 수 : " + PhotonNetwork.CountOfPlayers);
+            print("방 개수 : " + PhotonNetwork.CountOfRooms);
+            print("모든 방에 있는 인원 수 : " + PhotonNetwork.CountOfPlayersInRooms);
+            print("로비에 있는지? : " + PhotonNetwork.InLobby);
+            print("연결됐는지? : " + PhotonNetwork.IsConnected);
+        }
+    }
+    #endregion
 }
