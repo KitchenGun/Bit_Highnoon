851fb4682db792ee942b6365b63e414dbab8bbbd
SeoJiBeom <75297113+SeoJiBeom@users.noreply.github.com>
2021-02-01 16:23:40 +0900

    사운드 코드 작성, 옵션 패널

---
+++ Bit_Highnoon/Assets/Scripts/SceneManager/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider audioSlider;
+
+    public void AudioControl()
+    {
+        float sound = audioSlider.value;
+
+        if (sound == -40f)
+            audioMixer.SetFloat("Master", -80);
+        else
+            audioMixer.SetFloat("Master", sound);
+    }
+}
