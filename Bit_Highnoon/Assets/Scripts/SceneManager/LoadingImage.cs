e15cbd46a884df1d85b77877c1d8b4cc812b16f5
KitchenGun <kitchengun124@gmail.com>
2021-02-03 16:18:36 +0900

    32->64 빌드버전
    라이트, UI, 인트로 수정

---
+++ Bit_Highnoon/Assets/Scripts/SceneManager/LoadingImage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingImage : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] LoadingImages;
+
+    private void Start()
+    {
+        StartCoroutine(Loading());
+    }
+
+    private IEnumerator Loading()
+    {
+        int i = 0;
+        do
+        {
+            if (i < 5)
+            {
+                this.gameObject.GetComponent<Image>().sprite = LoadingImages[i];
+                i++;
+            }
+            else
+            {
+                this.gameObject.GetComponent<Image>().sprite = LoadingImages[i];
+                i =0;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+        while (true);
+    }
+
+}
